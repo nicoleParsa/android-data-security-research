6f1e7c9406b8a66c114f928b4e41ddf4eb5fe39d
nicoleParsa <38873458+nicoleParsa@users.noreply.github.com>
2020-02-09 22:01:32 +1100

    code refactor to enhance readability

--- app/src/main/java/com/example/password_protected_app/DataEncryptionByNicoleParsa.java
+++ app/src/main/java/com/example/password_protected_app/DataEncryptionByNicoleParsa.java
@@ -1,53 +1,51 @@
 package com.example.password_protected_app;
 
 import android.util.Log;
-
-import java.security.SecureRandom;
-import java.util.HashMap;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+import javax.crypto.SecretKeyFactory;
+import java.util.HashMap;
+import javax.crypto.spec.PBEKeySpec;
+
 
 public class DataEncryptionByNicoleParsa
 {
-    HashMap<String, byte[]> encryptBytes(byte[] plainTextBytes, String passwordString) {
-        HashMap<String, byte[]> map = new HashMap<String, byte[]>();
+    HashMap<String, byte[]> encryptBytes(byte[] plainTextBytes, String myPasswordString) {
+        HashMap<String, byte[]> myHashMap = new HashMap<String, byte[]>();
 
         try {
-            //Random salt for next step
-            SecureRandom random = new SecureRandom();
-            byte salt[] = new byte[256];
-            random.nextBytes(salt);
+            //generate salt using a random process
+            byte saltArray[] = new byte[256];
+            SecureRandom randomNum = new SecureRandom();
+            randomNum.nextBytes(saltArray);
 
-            //PBKDF2 - derive the key from the password, don't use passwords directly
-            char[] passwordChar = passwordString.toCharArray(); //Turn password into char[] array
-            PBEKeySpec pbKeySpec = new PBEKeySpec(passwordChar, salt, 1324, 256); //1324 iterations
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            byte[] keyBytes = secretKeyFactory.generateSecret(pbKeySpec).getEncoded();
-            SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+            //generate a secret key based on PBKDF2 on the password, not directly the password itself
+            char[] passCharArray = myPasswordString.toCharArray(); //converting to char array
+            PBEKeySpec keySpec = new PBEKeySpec(passCharArray, saltArray, 1324, 256); //iterations
+            SecretKeyFactory secretKey = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] keyBytesArray = secretKey.generateSecret(keySpec).getEncoded();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytesArray, "AES");
 
-            //Create initialization vector for AES
-            SecureRandom ivRandom = new SecureRandom(); //not caching previous seeded instance of SecureRandom
+            //initialize AES using random procedure for IV
+            SecureRandom ivRandomNum = new SecureRandom();
             byte[] iv = new byte[16];
-            ivRandom.nextBytes(iv);
-            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+            ivRandomNum.nextBytes(iv);
+            IvParameterSpec ivSpecParam = new IvParameterSpec(iv);
 
-            //Encrypt
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-            byte[] encrypted = cipher.doFinal(plainTextBytes);
+            //Encryption is done below:
+            Cipher cipherObj = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipherObj.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivSpecParam);
+            byte[] encryptedByteArray = cipherObj.doFinal(plainTextBytes);
 
-            map.put("salt", salt);
-            map.put("iv", iv);
-            map.put("encrypted", encrypted);
+            myHashMap.put("salt", saltArray);
+            myHashMap.put("iv", iv);
+            myHashMap.put("encrypted", encryptedByteArray);
         } catch (Exception e) {
-            //Log.e("MYAPP", "encryption exception", e);
             System.out.println("encrypt: " + e);
         }
-        return map;
+        return myHashMap;
 
     }
 }
